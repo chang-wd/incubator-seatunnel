948d588d06c10d8def5d740ff164cadb380aee66
CosmosNi <nijiahui_yewu@cmss.chinamobile.com>
2025-04-16 09:32:55 +0800

    [Feature][connector-elasticsearch] elasticsearch source support PIT (#9150)

--- seatunnel-connectors-v2/connector-elasticsearch/src/main/java/org/apache/seatunnel/connectors/seatunnel/elasticsearch/dto/source/PointInTimeResult.java
+++ seatunnel-connectors-v2/connector-elasticsearch/src/main/java/org/apache/seatunnel/connectors/seatunnel/elasticsearch/dto/source/PointInTimeResult.java
@@ -15,12 +15,33 @@
  * limitations under the License.
  */
 
-package org.apache.seatunnel.connectors.seatunnel.elasticsearch.config;
+package org.apache.seatunnel.connectors.seatunnel.elasticsearch.dto.source;
 
-public enum SearchTypeEnum {
-    /** Use Domain Specific Language (DSL) query */
-    DSL,
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    /** Use SQL query */
-    SQL
+import java.util.List;
+import java.util.Map;
+
+/** DTO for Elasticsearch Point-in-Time search results. */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PointInTimeResult {
+
+    /** The PIT ID used for this search */
+    private String pitId;
+
+    /** Documents returned by the search */
+    private List<Map<String, Object>> docs;
+
+    /** Total number of hits matching the query */
+    private long totalHits;
+
+    /** Sort values of the last document, used for pagination with search_after */
+    private Object[] searchAfter;
+
+    /** Whether there are more results to fetch */
+    private boolean hasMore;
 }
